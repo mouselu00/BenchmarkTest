8a5cbe230e243f94cc4f4d1f4fcdf9ead604089a
hayward lu <haywardlu00@gmail.com>
2024-08-25 18:58:56 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.ComponentModel.DataAnnotations.Schema;
+
+Console.WriteLine("Test Start!");
+var summary = BenchmarkRunner.Run<TestStage>();
+
+/// <summary>
+/// 測試舞台
+/// </summary>
+[MemoryDiagnoser]
+public class TestStage
+{
+
+    /// <summary>
+    /// 測試效能方法A
+    /// </summary>
+    [Benchmark]
+    public void NewEmptyList()
+    {
+        new List<PersonInformation>();
+    }
+
+    /// <summary>
+    /// 測試效能方法B
+    /// </summary>
+    [Benchmark]
+    public void NewEmptyEnumerable()
+    {
+
+        Enumerable.Empty<PersonInformation>();
+    }
+
+}
+
+
+/// <summary>
+/// 個人訊息類
+/// </summary>
+public class PersonInformation
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+
+}
+
